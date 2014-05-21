660c52e4f315831db9f871936f5c72bbd76a35b3
Ian Clelland <iclelland@chromium.org>
2014-05-21 15:14:22 -0400

    Add ARM library project

---
+++ libs/xwalk_core_library/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/iclelland/MCA4/xwalktest3/platforms/android/xwalk_core_library/bin/AndroidManifest.xml : \
+/Users/iclelland/MCA4/xwalktest3/platforms/android/xwalk_core_library/AndroidManifest.xml \
---
+++ libs/xwalk_core_library/src/org/chromium/base/library_loader/NativeLibraries.java
@@ -0,0 +1,16 @@
+
+
+
+
+package org.chromium.base.library_loader;
+
+public class NativeLibraries {
+    public static boolean USE_LINKER = false;
+    public static boolean ENABLE_LINKER_TESTS = false;
+    public static final String[] LIBRARIES
+= { "xwalkcore"}
+      ;
+    static String VERSION_NUMBER
+= ""
+      ;
+}
