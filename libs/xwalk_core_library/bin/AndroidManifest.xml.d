222da8be10f6cf28d16d1c5e2744f079139d5026
Ian Clelland <iclelland@chromium.org>
2014-06-03 10:23:01 -0400

    Update to XWalk 7.35.xxxx

--- libs/xwalk_core_library/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/iclelland/MCA4/xwalktest3/platforms/android/xwalk_core_library/bin/AndroidManifest.xml : \
-/Users/iclelland/MCA4/xwalktest3/platforms/android/xwalk_core_library/AndroidManifest.xml \
